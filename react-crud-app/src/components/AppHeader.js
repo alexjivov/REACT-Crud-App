2df0dbb1fa3b15a95bc0dfd28094f709a3dfaa3d
Alex Jivov <alexjivov@gwu.edu>
2018-08-14 19:22:43 -0400

    app working with css - add authentication

---
+++ react-crud-app/src/components/AppHeader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Card, Row, Col } from 'react-materialize';
+// import {
+//     AppBar,
+//     Toolbar,
+//     Typography,
+//   } from 'react-materialize';
+import "./AppHeader.css";
+const AppHeader = () => (
+//   <AppBar position="static">
+//     <Toolbar>
+//       <Typography variant="title" color="inherit">
+//         My React App
+//       </Typography>
+//     </Toolbar>
+//   </AppBar>
+<nav>
+    <div class="nav-wrapper">
+     <h3>  My React App</h3>
+    </div>
+  </nav>
+);
+
+
+export default AppHeader;
---
+++ react-crud-app/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button, Card, Row, Col } from 'react-materialize';
+import {
+    Typography,
+  } from 'react-materialize';
+export default (    ) => (
+  <Typography variant="display1">Welcome Home!</Typography>
+);
