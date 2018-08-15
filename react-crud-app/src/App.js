7635d7509ad5cd376781d49856e5621a984b3b4e
Alex Jivov <alexjivov@gwu.edu>
2018-08-14 20:14:15 -0400

    login functionality added but not working need to fix

--- react-crud-app/src/App.js
+++ react-crud-app/src/App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import AppHeader from './components/AppHeader';
 import Home from './pages/Home';
+import { Route } from 'react-router-dom';
+import { ImplicitCallback } from '@okta/okta-react';
+import loginButton from './components/loginButton';
+
 class App extends Component {
   render() {
     return (
       <div>
        <AppHeader/>
+       <Route exact path="/" component={Home} />
+     <Route path="/implicit/callback" component={ImplicitCallback} />
+        <loginButton/>
       {/* <Home/> */}
       <h1>Welcome Home!!</h1>
       </div>
+      
       // <div className="App">
       //   <header className="App-header">
       //     <img src={logo} className="App-logo" alt="logo" />
